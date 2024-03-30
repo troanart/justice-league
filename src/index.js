fef9b4c965969d264eea022740c9ee0df8ff9087
Artem Troyan <troanart@gmail.com>
2024-03-30 17:33:23 +0200

    COMPONENTS. Create and update structure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import mobileMenu from './js/mobile';
+import bodySkrollLock from './js/body-sckroll';
+import './scss/main.scss';
+
+var swiper = new Swiper('.mySwiper', {});
+
+mobileMenu();
+bodySkrollLock();
---
+++ src/js/body-sckroll.js
@@ -0,0 +1,26 @@
+const bodySkrollLock = () => {
+  var scrollPosition = window.scrollY;
+  const openBtnModal = document.querySelector('.js-open-menu');
+  const closeBtnModal = document.querySelector('.js-close-menu');
+
+  openBtnModal.addEventListener('click', lockScroll);
+  closeBtnModal.addEventListener('click', unlockScroll);
+
+  function lockScroll() {
+    document.body.style.overflow = 'hidden';
+    document.body.style.position = 'fixed';
+    document.body.style.top = -scrollPosition + 'px';
+  }
+
+  function unlockScroll() {
+    document.body.style.overflow = 'auto';
+    document.body.style.position = 'static';
+    document.body.style.top = 'auto';
+    window.scrollTo(0, scrollPosition);
+  }
+
+}
+
+export default bodySkrollLock
+
+
